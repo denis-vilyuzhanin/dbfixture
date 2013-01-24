5a38986cb28703be1e13308e7e3106d6bb5c503f
denis-vilyuzhanin <dvilyuzhanin@gmail.com>
2013-01-24 04:47:12 -0800

    [GREEN] Fix

--- src/main/java/dbfixture/Fixture.java
+++ src/main/java/dbfixture/Fixture.java
@@ -4,8 +4,14 @@ import java.util.Map;
 
 public class Fixture {
 
+	private Map<String, Object> values;
+	
+	public Fixture(Map<String, Object> values) {
+		this.values = values;
+	}
+
 	public Map<String, Object> getValues() {
-		return null;
+		return values;
 	}
 
 }
--- src/main/java/dbfixture/FixtureBuilder.java
+++ src/main/java/dbfixture/FixtureBuilder.java
@@ -1,9 +1,18 @@
 package dbfixture;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FixtureBuilder {
 
 	public Fixture build(TableMetadata table) {
-		return new Fixture();
+		Map<String, Object> values = new LinkedHashMap<String, Object>();
+		for(ColumnMetadata column : table.getColumns()) {
+			if (column.isNullable()) {
+				values.put(column.getName(), null);
+			}
+		}
+		return new Fixture(values);
 	}
 
 }
