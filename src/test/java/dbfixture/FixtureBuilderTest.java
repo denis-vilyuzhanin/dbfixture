1055c5b4650f0d6d9e5006217377c616fcd6161a
denis-vilyuzhanin <dvilyuzhanin@gmail.com>
2013-01-24 06:38:03 -0800

    [GREEN] Refactor test. Make them more readable

--- src/test/java/dbfixture/FixtureBuilderTest.java
+++ src/test/java/dbfixture/FixtureBuilderTest.java
@@ -3,6 +3,7 @@ package dbfixture;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
@@ -18,56 +19,59 @@ import static org.mockito.Mockito.*;
 public class FixtureBuilderTest {
 
 	FixtureBuilder builder;
-	
+
 	@Mock
 	TableMetadata table;
-	
-	@Mock
-	ColumnMetadata nullableNumericColumn;
-	
-	@Mock
-	ColumnMetadata numericColumn;
-	
-	@Mock
-	ColumnMetadata nullableCharacterColumn;
-	
-	@Mock
-	ColumnMetadata characterColumn;
-	
+
 	@Before
 	public void init() {
 		builder = new FixtureBuilder();
-		
-		mockColumn(nullableNumericColumn, "nullableNumericColumn", true, true, false);
-		mockColumn(numericColumn, "numericColumn", false, true, false);
-		mockColumn(nullableCharacterColumn, "nullableCharacterColumn", true, false, true);
-		mockColumn(characterColumn, "characterColumn", false, false, true);
-		
-		when(table.getColumns()).thenReturn(
-				Arrays.asList(nullableNumericColumn, 
-							  numericColumn, 
-							  nullableCharacterColumn, 
-							  characterColumn));
+		List<ColumnMetadata> columns = Arrays.asList(
+				nullable(numeric(column("nullableNumericColumn"))),
+				required(numeric(column("numericColumn"))),
+				nullable(character(column("nullableCharacterColumn"))),
+				required(character(column("characterColumn"))));
+		when(table.getColumns()).thenReturn(columns);
 	}
 
-	private void mockColumn(ColumnMetadata column, String name, boolean isNullable, boolean isNumeric, boolean isCharacter) {
-		when(column.getName()).thenReturn(name);
-		when(column.isNullable()).thenReturn(isNullable);
-		when(column.isNumeric()).thenReturn(isNumeric);
-		when(column.isCharacter()).thenReturn(isCharacter);
-	}
-	
 	@Test
-	public void  buildFixture() {
+	public void buildFixture() {
 		Fixture actualFixture = builder.build(table);
-		
+
 		Map<String, Object> expectedValues = new LinkedHashMap<String, Object>();
 		expectedValues.put("nullableNumericColumn", null);
 		expectedValues.put("numericColumn", 0);
 		expectedValues.put("nullableCharacterColumn", null);
 		expectedValues.put("characterColumn", "A");
-		
+
 		assertEquals(expectedValues, actualFixture.getValues());
 	}
-}
 
+	private ColumnMetadata column(String name) {
+		ColumnMetadata column = mock(ColumnMetadata.class);
+		when(column.getName()).thenReturn(name);
+		return column;
+	}
+
+	private ColumnMetadata numeric(ColumnMetadata column) {
+		when(column.isNumeric()).thenReturn(true);
+		when(column.isCharacter()).thenReturn(false);
+		return column;
+	}
+
+	private ColumnMetadata character(ColumnMetadata column) {
+		when(column.isNumeric()).thenReturn(false);
+		when(column.isCharacter()).thenReturn(true);
+		return column;
+	}
+
+	private ColumnMetadata nullable(ColumnMetadata column) {
+		when(column.isNullable()).thenReturn(true);
+		return column;
+	}
+
+	private ColumnMetadata required(ColumnMetadata column) {
+		when(column.isNullable()).thenReturn(false);
+		return column;
+	}
+}
